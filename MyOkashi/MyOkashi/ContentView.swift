c8d8bb325e02ca6b84f7177edf08e2cb53dae54c
Shingo Itoh <g2122006@fun.ac.jp>
2022-10-08 19:13:10 +0900

    add MyOkashi

---
+++ MyOkashi/MyOkashi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyOkashi
+//
+//  Created by Itou Shingo on 2022/10/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyOkashi/MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by Itou Shingo on 2022/10/08.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
